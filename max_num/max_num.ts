92185fac2dc244c34070d956a0038053e0d20aff
SeoDonggwon <ehdrnjsdl920@naver.com>
2022-08-03 19:22:42 +0900

    프로그래머스 가장큰수 typescript.ver 최종 코드

--- max_num/max_num.ts
+++ max_num/max_num.ts
@@ -1,20 +1,3 @@
-// export function Solution(numbers) {
-//     let answer: string ="";
-//     let str: string[] = new Array(numbers.length);
-//     let swap:string[]= [];
-//     //a, b를 비교해서 반환 값이 0보다 클 경우, b가 a보다 앞에 오도록 정렬한다.
-//
-//     numbers.sort((a, b)=>{
-//         a=a.toString();
-//         b=b.toString();
-//         return parseInt(b + a) - parseInt(a + b);
-//     });
-//     console.log(numbers);
-//     answer=numbers.join('');
-//
-//     return answer;
-// }
-
 export function max_num(numbers) {
     let answer: string = "";
     let str: string[] = new Array(numbers.length);
